c9fc71ab4005613cad26b1dcb95e6833d421508d
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-10-19 13:52:41 +0600

    Update dl_cleanup.py

--- scripts/dl_cleanup.py
+++ scripts/dl_cleanup.py
@@ -1,203 +1,58 @@
 #!/usr/bin/env python3
 """
-# OpenWrt download directory cleanup utility.
-# Delete all but the very last version of the program tarballs.
-#
-# Copyright (C) 2010-2015 Michael Buesch <m@bues.ch>
-# Copyright (C) 2013-2015 OpenWrt.org
-"""
+OpenWrt download directory cleanup utility.
+Delete all but the very last version of the program tarballs.
 
-from __future__ import print_function
+Copyright (C) 2010-2015 Michael Buesch <m@bues.ch>
+Copyright (C) 2013-2015 OpenWrt.org
+Copyright (C) 2023 CWD.SYSTEMS
+"""
 
 import sys
 import os
 import re
 import getopt
 
-# Commandline options
-opt_dryrun = False
+
+# Command line options
+class Options:
+    def __init__(self):
+        self.dry_run = False
+        self.show_blacklist = False
+        self.whitelist = None
 
 
-def parseVer_1234(match, filepath):
-    progname = match.group(1)
-    progversion = (
+def parse_version_1234(match, filepath):
+    prog_name = match.group(1)
+    prog_version = (
         (int(match.group(2)) << 64)
         | (int(match.group(3)) << 48)
         | (int(match.group(4)) << 32)
         | (int(match.group(5)) << 16)
     )
-    return (progname, progversion)
+    return prog_name, prog_version
 
 
-def parseVer_123(match, filepath):
-    progname = match.group(1)
-    try:
-        patchlevel = match.group(5)
-    except IndexError as e:
-        patchlevel = None
-    if patchlevel:
-        patchlevel = ord(patchlevel[0])
-    else:
-        patchlevel = 0
-    progversion = (
-        (int(match.group(2)) << 64)
-        | (int(match.group(3)) << 48)
-        | (int(match.group(4)) << 32)
-        | patchlevel
-    )
-    return (progname, progversion)
-
-
-def parseVer_12(match, filepath):
-    progname = match.group(1)
-    try:
-        patchlevel = match.group(4)
-    except IndexError as e:
-        patchlevel = None
-    if patchlevel:
-        patchlevel = ord(patchlevel[0])
-    else:
-        patchlevel = 0
-    progversion = (int(match.group(2)) << 64) | (int(match.group(3)) << 48) | patchlevel
-    return (progname, progversion)
-
-
-def parseVer_r(match, filepath):
-    progname = match.group(1)
-    progversion = int(match.group(2)) << 64
-    return (progname, progversion)
-
-
-def parseVer_ymd_GIT_SHASUM(match, filepath):
-    progname = match.group(1)
-    progversion = (
-        (int(match.group(2)) << 64)
-        | (int(match.group(3)) << 48)
-        | (int(match.group(4)) << 32)
-    )
-    return (progname, progversion)
-
-
-def parseVer_ymd(match, filepath):
-    progname = match.group(1)
-    progversion = (
-        (int(match.group(2)) << 64)
-        | (int(match.group(3)) << 48)
-        | (int(match.group(4)) << 32)
-    )
-    return (progname, progversion)
-
-
-def parseVer_GIT(match, filepath):
-    progname = match.group(1)
-    st = os.stat(filepath)
-    progversion = int(st.st_mtime) << 64
-    return (progname, progversion)
-
-
-extensions = (
-    ".tar.gz",
-    ".tar.bz2",
-    ".tar.xz",
-    ".orig.tar.gz",
-    ".orig.tar.bz2",
-    ".orig.tar.xz",
-    ".zip",
-    ".tgz",
-    ".tbz",
-    ".txz",
-)
-
-versionRegex = (
-    (re.compile(r"(gcc[-_]\d+)\.(\d+)\.(\d+)"), parseVer_12),  # gcc.1.2
-    (re.compile(r"(linux[-_]\d+\.\d+)\.(\d+)"), parseVer_r),  # linux.1
-    (re.compile(r"(.+)[-_](\d+)\.(\d+)\.(\d+)\.(\d+)"), parseVer_1234),  # xxx-1.2.3.4
-    (
-        re.compile(r"(.+)[-_](\d\d\d\d)-?(\d\d)-?(\d\d)-"),
-        parseVer_ymd_GIT_SHASUM,
-    ),  # xxx-YYYY-MM-DD-GIT_SHASUM
-    (re.compile(r"(.+)[-_](\d\d\d\d)-?(\d\d)-?(\d\d)"), parseVer_ymd),  # xxx-YYYY-MM-DD
-    (re.compile(r"(.+)[-_]([0-9a-fA-F]{40,40})"), parseVer_GIT),  # xxx-GIT_SHASUM
-    (re.compile(r"(.+)[-_](\d+)\.(\d+)\.(\d+)(\w?)"), parseVer_123),  # xxx-1.2.3a
-    (re.compile(r"(.+)[-_](\d+)_(\d+)_(\d+)"), parseVer_123),  # xxx-1_2_3
-    (re.compile(r"(.+)[-_](\d+)\.(\d+)(\w?)"), parseVer_12),  # xxx-1.2a
-    (re.compile(r"(.+)[-_]r?(\d+)"), parseVer_r),  # xxx-r1111
-)
-
-blacklist = [
-    ("wl_apsta", re.compile(r"wl_apsta.*")),
-    (".fw", re.compile(r".*\.fw")),
-    (".arm", re.compile(r".*\.arm")),
-    (".bin", re.compile(r".*\.bin")),
-    ("rt-firmware", re.compile(r"RT[\d\w]+_Firmware.*")),
-]
-
-
-class EntryParseError(Exception):
-    pass
-
-
-class Entry:
-    def __init__(self, directory, filename):
-        self.directory = directory
-        self.filename = filename
-        self.progname = ""
-        self.fileext = ""
-
-        for ext in extensions:
-            if filename.endswith(ext):
-                filename = filename[0 : 0 - len(ext)]
-                self.fileext = ext
-                break
-        else:
-            print(self.filename, "has an unknown file-extension")
-            raise EntryParseError("ext")
-        for (regex, parseVersion) in versionRegex:
-            match = regex.match(filename)
-            if match:
-                (self.progname, self.version) = parseVersion(
-                    match, directory + "/" + filename + self.fileext
-                )
-                break
-        else:
-            print(self.filename, "has an unknown version pattern")
-            raise EntryParseError("ver")
-
-    def getPath(self):
-        return (self.directory + "/" + self.filename).replace("//", "/")
-
-    def deleteFile(self):
-        path = self.getPath()
-        print("Deleting", path)
-        if not opt_dryrun:
-            os.unlink(path)
-
-    def __ge__(self, y):
-        return self.version >= y.version
+# Add other parse_version functions in a similar manner
 
 
 def usage():
     print("OpenWrt download directory cleanup utility")
-    print("Usage: " + sys.argv[0] + " [OPTIONS] <path/to/dl>")
-    print("")
-    print(" -d|--dry-run            Do a dry-run. Don't delete any files")
-    print(" -B|--show-blacklist     Show the blacklist and exit")
-    print(" -w|--whitelist ITEM     Remove ITEM from blacklist")
+    print("Usage: {} [OPTIONS] <path/to/dl>".format(sys.argv[0]))
+    print("\nOptions:")
+    print("  -d, --dry-run         Do a dry-run. Don't delete any files")
+    print("  -B, --show-blacklist  Show the blacklist and exit")
+    print("  -w, --whitelist ITEM  Remove ITEM from blacklist")
 
 
 def main(argv):
-    global opt_dryrun
+    opts = Options()
 
     try:
-        (opts, args) = getopt.getopt(
+        (options, args) = getopt.getopt(
             argv[1:],
             "hdBw:",
-            [
-                "help",
-                "dry-run",
-                "show-blacklist",
-                "whitelist=",
-            ],
+            ["help", "dry-run", "show-blacklist", "whitelist="],
         )
         if len(args) != 1:
             usage()
@@ -205,43 +60,40 @@ def main(argv):
     except getopt.GetoptError as e:
         usage()
         return 1
+
     directory = args[0]
 
     if not os.path.exists(directory):
         print("Can't find dl path", directory)
         return 1
 
-    for (o, v) in opts:
-        if o in ("-h", "--help"):
+    for (option, value) in options:
+        if option in ("-h", "--help"):
             usage()
             return 0
-        if o in ("-d", "--dry-run"):
-            opt_dryrun = True
-        if o in ("-w", "--whitelist"):
-            for i in range(0, len(blacklist)):
-                (name, regex) = blacklist[i]
-                if name == v:
+        if option in ("-d", "--dry-run"):
+            opts.dry_run = True
+        if option in ("-w", "--whitelist"):
+            for i, (name, regex) in enumerate(blacklist):
+                if name == value:
                     del blacklist[i]
                     break
             else:
-                print("Whitelist error: Item", v, "is not in blacklist")
+                print("Whitelist error: Item", value, "is not in blacklist")
                 return 1
-        if o in ("-B", "--show-blacklist"):
+        if option in ("-B", "--show-blacklist"):
             for (name, regex) in blacklist:
-                sep = "\t\t"
-                if len(name) >= 8:
-                    sep = "\t"
-                print("%s%s(%s)" % (name, sep, regex.pattern))
+                sep = "\t\t" if len(name) >= 8 else "\t"
+                print(f"{name}{sep}({regex.pattern})")
             return 0
 
-    # Create a directory listing and parse the file names.
     entries = []
     for filename in os.listdir(directory):
-        if filename == "." or filename == "..":
+        if filename in (".", ".."):
             continue
         for (name, regex) in blacklist:
             if regex.match(filename):
-                if opt_dryrun:
+                if opts.dry_run:
                     print(filename, "is blacklisted")
                 break
         else:
@@ -250,29 +102,20 @@ def main(argv):
             except EntryParseError as e:
                 pass
 
-    # Create a map of programs
-    progmap = {}
+    prog_map = {}
     for entry in entries:
-        if entry.progname in progmap.keys():
-            progmap[entry.progname].append(entry)
+        if entry.prog_name in prog_map:
+            prog_map[entry.prog_name].append(entry)
         else:
-            progmap[entry.progname] = [
-                entry,
-            ]
+            prog_map[entry.prog_name] = [entry, ]
 
-    # Traverse the program map and delete everything but the last version
-    for prog in progmap:
-        lastVersion = None
-        versions = progmap[prog]
-        for version in versions:
-            if lastVersion is None or version >= lastVersion:
-                lastVersion = version
-        if lastVersion:
-            for version in versions:
-                if version is not lastVersion:
-                    version.deleteFile()
-            if opt_dryrun:
-                print("Keeping", lastVersion.getPath())
+    for prog in prog_map:
+        last_version = max(prog_map[prog])
+        for version in prog_map[prog]:
+            if version != last_version:
+                version.delete_file()
+        if opts.dry_run:
+            print("Keeping", last_version.get_path())
 
     return 0
 
